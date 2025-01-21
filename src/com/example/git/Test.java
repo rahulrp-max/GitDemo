78cc33d4f5d876ba82c516d259ebd2eac03722ea
rahulrp-max <rahulrp061@gmail.com>
2025-01-21 12:55:09 +0530

    hella;jsfdklja

--- src/com/example/git/Test.java
+++ src/com/example/git/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.err.println("hello world");
-		System.out.println();
+		System.out.println("asdfalskd;jfka;sldkfj;aks");
 	}
 }
